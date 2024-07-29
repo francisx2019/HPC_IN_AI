3a140a25ad150394eec1f2410475ca87ca588ab1
francis <54396443+francisx2019@users.noreply.github.com>
2024-07-29 08:56:27 +0800

    "更新transformer代码"

--- cuda-learn-01/transformer_torch.py
+++ cuda-learn-01/transformer_torch.py
@@ -1,9 +1,9 @@
 import torch
 import torch.nn as nn
 
-class selfAttention(nn.Module):
+class SelfAttention(nn.Module):
     def __init__(self, embed_size, heads):
-        super(selfAttention, self).__init__()
+        super(SelfAttention, self).__init__()
         self.embed_size = embed_size
         self.heads = heads
         self.head_dim = embed_size // heads
@@ -48,3 +48,141 @@ class selfAttention(nn.Module):
         
         return self.fc_out(out) # (batch_size, query_len, embed_size)
         
+
+class TransformerBlock(nn.Module):
+    def __init__(self, embed_size, heads, dropout, forward_expansion):
+        super(TransformerBlock, self).__init__()
+        self.attention = SelfAttention(embed_size, heads)
+        self.norm1 = nn.LayerNorm(embed_size)
+        self.norm2 = nn.LayerNorm(embed_size)
+        self.feed_forward = nn.Sequential(
+            nn.Linear(embed_size, forward_expansion * embed_size),
+            nn.ReLU(),
+            nn.Linear(forward_expansion * embed_size, embed_size)
+        )
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, value, key, query, mask):
+        attention = self.attention(value, key, query, mask)
+        # add residual connection
+        x = self.dropout(self.norm1(attention + query))
+        forward = self.feed_forward(x)
+        out = self.dropout(self.norm2(forward + x))
+        return out
+    
+
+class Encoder(nn.Module):
+    def __init__(self, src_vocab_size, embed_size, num_layers, heads, device, forward_expansion, dropout, max_length):
+        super(Encoder, self).__init__()
+        self.embed_size = embed_size
+        self.device = device
+        self.word_embedding = nn.Embedding(src_vocab_size, embed_size)
+        self.position_embedding = nn.Embedding(max_length, embed_size)
+
+        self.layers = nn.ModuleList(
+            [TransformerBlock(embed_size, heads, dropout, forward_expansion) for _ in range(num_layers)]
+        )
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, mask):
+        N, seq_len = x.shape
+        positions = torch.arange(0, seq_len).expand(N, seq_len).to(self.device)
+        out = self.dropout(self.word_embedding(x) + self.position_embedding(positions))
+        # in the encoder the value, key and query are the same
+        for layer in self.layers:
+            out = layer(out, out, out, mask)
+        return out
+    
+
+class DecoderBlock(nn.Module):
+    def __init__(self, embed_size, heads, forward_expansion, dropout, device):
+        super(DecoderBlock, self).__init__()
+        self.attention = SelfAttention(embed_size, heads)
+        self.norm = nn.LayerNorm(embed_size)
+        self.transformerBlock = TransformerBlock(embed_size, heads, dropout, forward_expansion)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, value, key, src_mask, trg_mask):
+        attention = self.attention(x, x, x, trg_mask)
+        query = self.dropout(self.norm(attention + x))
+        out = self.transformerBlock(value, key, query, src_mask)
+        return out
+    
+
+class Decoder(nn.Module):
+    def __init__(self, trg_vocab_size, embed_size, num_layers, heads, forward_expansion, dropout, device, max_length):
+        super(Decoder, self).__init__()
+        self.device = device
+        self.word_embedding = nn.Embedding(trg_vocab_size, embed_size)
+        self.position_embedding = nn.Embedding(max_length, embed_size)
+        self.layers = nn.ModuleList(
+            [DecoderBlock(embed_size, heads, forward_expansion, dropout, device) for _ in range(num_layers)]
+        )
+        self.fc_out = nn.Linear(embed_size, trg_vocab_size)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, enc_out, src_mask, trg_mask):
+        N, seq_len = x.shape
+        positions = torch.arange(0, seq_len).expand(N, seq_len).to(self.device)
+        x = self.dropout((self.word_embedding(x) + self.position_embedding(positions)))
+        
+        for layer in self.layers:
+            x = layer(x, enc_out, enc_out, src_mask, trg_mask)
+        out = self.fc_out(x)
+        return out
+    
+class Transformer(nn.Module):
+    def __init__(self, src_vocab_size, trg_vocab_size, src_pad_idx, trg_pad_idx, embed_size=256, num_layers=6, forward_expansion=4, heads=8, dropout=0, device='cuda', max_length=100):
+        super(Transformer, self).__init__()
+        self.encoder = Encoder(src_vocab_size, embed_size, num_layers, heads, device, forward_expansion, dropout, max_length)
+        self.decoder = Decoder(trg_vocab_size, embed_size, num_layers, heads, forward_expansion, dropout, device, max_length)
+        self.src_pad_idx, self.trg_pad_idx = src_pad_idx, trg_pad_idx
+        self.device = device
+
+    def make_src_mask(self, src):
+        src_mask = (src != self.src_pad_idx).unsqueeze(1).unsqueeze(2)
+        return src_mask.to(self.device)
+
+    def make_trg_mask(self, trg):
+        N, trg_len = trg.shape
+        trg_mask = torch.tril(torch.ones((trg_len, trg_len))).expand(N, 1, trg_len, trg_len)
+        return trg_mask.to(self.device)
+
+    def forward(self, src, trg):
+        src_mask = self.make_src_mask(src)
+        trg_mask = self.make_trg_mask(trg)
+        enc_src = self.encoder(src, src_mask)
+        out = self.decoder(trg, enc_src, src_mask, trg_mask)
+        return out
+
+
+def trace_handler(prof):
+    print(prof.key_averages().table(sort_by="self_cuda_time_total", row_limit=-1))
+    prof.export_chrome_trace("/tmp/test_pytorch_trace_" + str(prof.step_num) + ".json")
+
+
+if __name__ == '__main__':
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(device)
+    model = Transformer(10, 10, 0, 0, device=device).to(device)
+    print(model)
+    x = torch.tensor([[1, 2, 3, 0, 0, 0], [1, 2, 0, 0, 0, 0]]).to(device)
+    trg = torch.tensor([[1, 2, 3, 4, 0, 0], [1, 0, 0, 0, 0, 0]]).to(device)
+    print("--------------------------------------------")
+    print("profiling transformer")
+    print("--------------------------------------------")
+    with torch.autograd.profiler.profile(use_cuda=True) as prof:
+        out = model(x, trg[:, :-1])
+    print(prof.key_averages().table(sort_by="cuda_time_total", row_limit=10))
+
+    # 使用pytorch自带的profiler接口，生成json图展示cpu和gpu调用逻辑
+    with torch.profiler.profile(
+        activities=[torch.profiler.ProfilerActivity.CPU, torch.profiler.ProfilerActivity.CUDA],
+        schedule=torch.profiler.schedule(wait=1, warmup=1, active=2, repeat=1),
+        on_trace_ready=trace_handler
+        # on_trace_ready=torch.profiler.tensorboard_trace_handler("./log")
+    ) as p:
+        for iter in range(10):
+            out = model(x, trg[:, :-1])
+            # 发出信号给profile开始下一个迭代
+            p.step()
